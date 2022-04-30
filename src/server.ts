d90ff1258d13613aa10fa9a471e9bb2c155ae1b0
Caio Oda <rm87707@fiap.com.br>
2022-04-30 20:20:13 -0300

    #v2.2.0

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,71 @@ app.get('/user', function (request: Request, response: Response) {
     }
 })
 
+app.get('/user/name', function (request: Request, response: Response) {
+    const name = String(request.query.name)
+    try {
+        prismaClient.user.findMany({
+            where: {name: name}
+        }).then(
+            (r: any) => response.send(r)
+        )
+    } catch (error: any) {
+        response.status(501).json({ error: error })
+    }
+})
+
+app.get('/user/email', function (request: Request, response: Response) {
+    const email = String(request.query.email)
+    try {
+        prismaClient.user.findMany({
+            where: {email: email}
+        }).then(
+            (r: any) => response.send(r)
+        )
+    } catch (error: any) {
+        response.status(501).json({ error: error })
+    }
+})
+
+app.get('/user/cpf', function (request: Request, response: Response) {
+    const cpf = String(request.query.cpf)
+    try {
+        prismaClient.user.findMany({
+            where: {cpf: cpf}
+        }).then(
+            (r: any) => response.send(r)
+        )
+    } catch (error: any) {
+        response.status(501).json({ error: error })
+    }
+})
+
+app.get('/user/skills', function (request: Request, response: Response) {
+    const skills = String(request.query.skills)
+    try {
+        prismaClient.user.findMany({
+            where: {skills: skills}
+        }).then(
+            (r: any) => response.send(r)
+        )
+    } catch (error: any) {
+        response.status(501).json({ error: error })
+    }
+})
+
+app.get('/user/certifications', function (request: Request, response: Response) {
+    const certifications = String(request.query.certifications)
+    try {
+        prismaClient.user.findMany({
+            where: {certifications: certifications}
+        }).then(
+            (r: any) => response.send(r)
+        )
+    } catch (error: any) {
+        response.status(501).json({ error: error })
+    }
+})
+
 app.post('/student', function (request: Request, response: Response) {
     const { user_id, name } = request.body
     prismaClient.student.create({
@@ -101,18 +166,28 @@ app.post('/course', function (request: Request, response: Response) {
 })
 
 app.post('/user', function (request: Request, response: Response) {
-    const { email, password } = request.body
+    const { email, phone, gender, skills, birthday, cpf, certifications, name } = request.body
+    console.log(request.body)
     prismaClient.user.create({
         data: {
             email: email,
-            password: password
+            phone: phone,
+            gender: gender,
+            skills: skills,
+            name: name,
+            certifications: certifications,
+            birthday: birthday,
+            cpf: cpf
         }
     }).then(
         (r: any) => response.json(r)
     ).catch(
-        (r: any) => response.status(501).json({
-            error: r
-        })
+        (r: any) => {
+            console.log("ERROR -->", r)
+            response.status(501).json({
+                error: r
+            })
+        }
     )
 })
 
@@ -164,21 +239,21 @@ app.put('/course', function (request: Request, response: Response) {
     )
 })
 
-app.put('/user', function (request: Request, response: Response) {
-    const { id, password } = request.body
-    prismaClient.user.update({
-        where: { id: id },
-        data: {
-            password: password
-        }
-    }).then(
-        (r: any) => response.json(r)
-    ).catch(
-        (r: any) => response.status(501).json({
-            error: r
-        })
-    )
-})
+// app.put('/user', function (request: Request, response: Response) {
+//     const { id, password } = request.body
+//     prismaClient.user.update({
+//         where: { id: id },
+//         data: {
+//             password: password
+//         }
+//     }).then(
+//         (r: any) => response.json(r)
+//     ).catch(
+//         (r: any) => response.status(501).json({
+//             error: r
+//         })
+//     )
+// })
 
 app.delete('/student', function (request: Request, response: Response) {
     const { id } = request.body
